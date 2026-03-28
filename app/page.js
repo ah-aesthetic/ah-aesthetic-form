b2aa614c2f617bcf472fce9983893702eec6c970
ah-aesthetic <annagri199527@gmail.com>
2026-03-28 09:08:22 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+"use client";
+import BotoxIntakeForm from './BotoxIntakeForm';
+
+export default function Home() {
+  return (
+    <main style={{ 
+      minHeight: '100vh', 
+      backgroundColor: '#FDF9F7',
+      padding: '20px 0'
+    }}>
+      <BotoxIntakeForm />
+    </main>
+  );
+}
